842b3819b263d57878c9476c3f7357c7d83e78e3
TimotheeRapin <61775714+TimotheeRapin@users.noreply.github.com>
2021-11-30 14:46:19 +0100

    Projet Terres Créé

---
+++ Terres/Pictures.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pictures;
+
+class Pictures
+{
+    //<editor-fold desc="private attributes">
+
+    //</editor-fold>
+
+    //<editor-fold desc="public methods">
+
+    //</editor-fold>
+
+    //<editor-fold desc="private methods">
+
+    //</editor-fold>
+}
---
+++ Terres/Users.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Users;
+
+class Users
+{
+    //<editor-fold desc="private attributes">
+
+    //</editor-fold>
+
+    //<editor-fold desc="public methods">
+
+    //</editor-fold>
+
+    //<editor-fold desc="private methods">
+
+    //</editor-fold>
+}
